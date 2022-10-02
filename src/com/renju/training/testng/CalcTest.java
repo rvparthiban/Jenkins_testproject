fb34510282a2919995b98675f793e898d203ee61
Parthiban Ramasamy Vijayananthan <parthibanramasamyvijayananthan@Parthibans-MBP.fios-router.home>
2022-10-02 18:20:08 -0400

    add

---
+++ src/com/renju/training/testng/CalcTest.java
@@ -0,0 +1,18 @@
+package com.renju.training.testng;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+public class CalcTest {
+	@Test
+	public void addTest() {
+		Calculator myCalc = new Calculator();
+		AssertJUnit.assertEquals(10, myCalc.addNumbers(5, 5));
+	}
+
+	@Test
+	public void subtractTest() {
+		Calculator myCalc = new Calculator();
+		AssertJUnit.assertEquals(5, myCalc.subtractNumbers(10, 5));
+	}
+}
